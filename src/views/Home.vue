48478f471a59202558428892f83214ddc0cb71a1
Ashish Mishra <ashishk861@gmail.com>
2020-07-20 14:07:24 -0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>HomePage</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque enim consectetur! Animi, magni sunt minima voluptates perferendis alias inventore neque porro vitae iste. Sed distinctio nemo ipsam cum ex!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
