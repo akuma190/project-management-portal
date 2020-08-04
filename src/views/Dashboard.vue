62d08c7297e7a7fa1d4b8133b2ca21affed3ed9d
Ashish Mishra <ashishk861@gmail.com>
2020-08-04 00:10:13 -0400

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="dashboard">
    <h1 class="subheading grey--text">Dashboard</h1>
+
     <v-container class="my-5">
-      
-      <v-layout row wrap>
+
+      <v-card-flat class="pa3">
+        <v-layout row wrap class="pink lighten-5 "> 
         <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
+          <div class="caption grey--text">Project Title</div>
+          <div>Vue JS Portal</div>
+        </v-flex>
+         <v-flex xs6 sm4 md2>
+          <div class="caption grey--text">Person</div>
+          <div>Ashish Kumar</div>
+        </v-flex>
+         <v-flex xs6 sm4 md2>
+          <div class="caption grey--text">Due By:</div>
+          <div>1st Jan 2020</div>
+        </v-flex>
+         <v-flex xs2 sm4 md2>
+          <div class="caption grey--text">Status</div>
+          <div>Ongoing</div>
         </v-flex>
       </v-layout>
+
+      </v-card-flat>
+      
+
     </v-container>
+
+
   </div>
 </template>
 
