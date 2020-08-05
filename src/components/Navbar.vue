7ad35159f97f8352c4bd8f992cb720fb5952cfce
Ashish Mishra <ashishk861@gmail.com>
2020-08-05 16:02:49 -0400

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,42 @@
             <span>Mangement</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+
+            <!-- dropdown menu -->
+      <v-menu offset-y>
+        <v-btn flat slot="activator" color="grey">
+          <v-icon left>mdi-home</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+<!-- 
+      <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Dropdown
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+         
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu> -->
+
             <v-btn right flat color="grey">
                 <span>Sign Out</span>
                 <v-icon right large color="orange darken-2">mdi-arrow-up-bold-box-outline</v-icon>
@@ -20,6 +56,9 @@
             <img class="text-lg-center" src="/image1.png">
           </v-avatar>
           <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup/>
         </v-flex>
       </v-layout>
            <v-list>
@@ -37,7 +76,9 @@
 </template>
 
 <script>
+import Popup from './Popup'
 export default {
+  components: { Popup },
      data(){
          return{
              drawer:false,
