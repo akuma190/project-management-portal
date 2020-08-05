08e47364977baaa43f693364ea806b7ff464dde1
Ashish Mishra <ashishk861@gmail.com>
2020-08-05 01:35:08 -0400

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,27 +4,47 @@
 
     <v-container class="my-5">
 
-      <v-card-flat class="pa3">
-        <v-layout row wrap class="pink lighten-5 "> 
-        <v-flex xs12 md6>
-          <div class="caption grey--text">Project Title</div>
-          <div>Vue JS Portal</div>
-        </v-flex>
-         <v-flex xs6 sm4 md2>
-          <div class="caption grey--text">Person</div>
-          <div>Ashish Kumar</div>
-        </v-flex>
-         <v-flex xs6 sm4 md2>
-          <div class="caption grey--text">Due By:</div>
-          <div>1st Jan 2020</div>
-        </v-flex>
-         <v-flex xs2 sm4 md2>
-          <div class="caption grey--text">Status</div>
-          <div>Ongoing</div>
-        </v-flex>
+      <v-layout row justify-start class="mb-3">
+       <v-tooltip left>
+      <template v-slot:activator="{ }">
+          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+            <v-icon small left>mdi-home</v-icon>
+            <span class="caption text-lowercase">By project name</span>
+          </v-btn>
+         </template>
+      <span>Left tooltip</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+            <v-icon small left>mdi-home</v-icon>
+            <span class="caption text-lowercase">By Person</span>
+          </v-btn>
+          <span>Sort by project author</span>
+        </v-tooltip>
       </v-layout>
 
-      </v-card-flat>
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project title</div>
+            <div>{{ project.title }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>{{ project.due }}</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="right">
+              <v-chip small :class="`${project.status} white--text my-2 caption`">{{ project.status }}</v-chip>
+            </div>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+      </v-card>
       
 
     </v-container>
@@ -38,6 +58,41 @@
 
 
 export default {
- 
+ data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'Ashish', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create the Back End', person: 'Kumar', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Consider the Scalabality', person: 'Mishra', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a discussion forum', person: 'Shalu', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  },
+   methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+    }
+  }
 }
 </script>
+
+<style>
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+.v-chip.complete{
+  color: #3cd1c2;
+}
+.v-chip.ongoing{
+  color: #ffaa2c;
+}
+.v-chip.overdue{
+  color: #f83e70;
+}
+</style>
